cffc3ae0a5dfaa1487ae25fb693e9331970a64e8
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-07-20 12:31:18 +0530

    add: CMake reset script

--- reset-build.sh
+++ reset-build.sh
@@ -3,3 +3,4 @@
 rm -rf cmake-build-debug CMakeFiles cmake_install.cmake CMakeCache.txt Makefile
 rm -rf engine/CMakeFiles engine/cmake_install.cmake engine/Makefile
 rm -rf test/CMakeFiles test/cmake_install.cmake test/Makefile
+rm -rf out
