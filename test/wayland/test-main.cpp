007efadb7f0f1a59b04296a82ca9c7dcfc53b6fd
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-07-20 19:37:12 +0530

    add: XDG Surface draw

--- test/wayland/test-main.cpp
+++ test/wayland/test-main.cpp
@@ -1,13 +1,104 @@
 #include <iostream>
+#include <unistd.h>
+
 #include <wayland-client.h>
 #include "engine/platform/logger.hpp"
 #include "wl-client.hpp"
 
+void xdg_surface_configure_handler
+        (
+                void *data,
+                struct zxdg_surface_v6 *xdg_surface,
+                uint32_t serial
+        ) {
+    vtrs::Logger::info("Ack surface configure");
+    zxdg_surface_v6_ack_configure(xdg_surface, serial);
+}
+
+const struct zxdg_surface_v6_listener xdg_surface_listener = {
+        .configure = xdg_surface_configure_handler
+};
+
+//////////////////////////////////////////////////////////
+
+void xdg_toplevel_configure_handler
+        (
+                void *data,
+                struct zxdg_toplevel_v6 *xdg_toplevel,
+                int32_t width,
+                int32_t height,
+                struct wl_array *states
+        ) {
+    vtrs::Logger::info("configure toplevel:", width, height);
+}
+
+void xdg_toplevel_close_handler
+        (
+                void *data,
+                struct zxdg_toplevel_v6 *xdg_toplevel
+        ) {
+    vtrs::Logger::info("Close TopLevel");
+}
+
+const struct zxdg_toplevel_v6_listener xdg_toplevel_listener = {
+        .configure = xdg_toplevel_configure_handler,
+        .close = xdg_toplevel_close_handler
+};
+
 int main() {
-    TT();
+    vtrs::Logger::info("Test: Hello-Wayland");
 
     vtest::WLClient* client = vtest::WLClient::getInstance();
+    zxdg_surface_v6_add_listener(client->getXDGSurface(), &xdg_surface_listener, nullptr);
+    zxdg_toplevel_v6_add_listener(client->getXDGTopLevel(), &xdg_toplevel_listener, nullptr);
+
+    client->commitSurface();
+
+    int width = 200;
+    int height = 200;
+    int stride = width * 4;
+    int size = stride * height;  // bytes
+
+    unsigned char* buffer_data = client->getBufferData();
+
+    //*
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+
+            struct pixel {
+                // little-endian ARGB
+                unsigned char blue;
+                unsigned char green;
+                unsigned char red;
+                unsigned char alpha;
+            } *px = (struct pixel *) (buffer_data + y * stride + x * 4);
+
+            // draw a cross
+            if ((80 < x && x < 120) || (80 < y && y < 120)) {
+                // gradient from blue at the top to white at the bottom
+                px->alpha = 255;
+                px->red = y / height * 255;
+                px->green = px->red;
+                px->blue = 255;
+            } else {
+                // transparent
+                px->alpha = 0;
+            }
+        }
+    }// */
+    client->commitSurface();
+
+    vtrs::Logger::info("Preparing to dispatch");
+
+    for(int loop = 0; loop < 5; loop++) {
+        vtrs::Logger::info("Ping", loop);
+        vtest::WLClient::dispatch();
+        sleep(1);
+    }
+
     delete client;
 
+    vtest::WLClient::destroy();
     return 0;
 }
--- test/wayland/wl-client.cpp
+++ test/wayland/wl-client.cpp
@@ -1,8 +1,116 @@
+#include <syscall.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdexcept>
 #include "engine/platform/logger.hpp"
+#include "xdg-shell.h"
 #include "wl-client.hpp"
 
+struct wl_display* vtest::WLClient::s_display = nullptr;
+struct wl_registry* vtest::WLClient::s_registry = nullptr;
+struct wl_shm* vtest::WLClient::s_sharedMem = nullptr;
+struct wl_compositor* vtest::WLClient::s_compositor = nullptr;
+struct zxdg_shell_v6* vtest::WLClient::s_xdgShell = nullptr;
+
+struct wl_registry_listener vtest::WLClient::s_regListener = {
+    vtest::WLClient::s_regHandler,
+    vtest::WLClient::s_regRemover
+};
+
 vtest::WLClient *vtest::WLClient::getInstance() {
+    if (s_display != nullptr) {
+        return new WLClient();
+    }
+
+    s_display = wl_display_connect(nullptr);
+
+    if (s_display == nullptr) {
+        throw std::runtime_error("Unable to connect to wl_display.");
+    }
+
+    s_registry = wl_display_get_registry(s_display);
+    wl_registry_add_listener(s_registry, &s_regListener, nullptr);
+
+    wl_display_roundtrip(s_display);
+
     return new WLClient();
 }
 
-vtest::WLClient::WLClient() = default;
+void vtest::WLClient::destroy() {
+    if (s_xdgShell != nullptr) {
+        vtrs::Logger::info("Destroy zxdg_shell_v6.");
+        zxdg_shell_v6_destroy(s_xdgShell);
+    }
+
+    if (s_display != nullptr) {
+        vtrs::Logger::info("Disconnect wl_display.");
+        wl_display_disconnect(s_display);
+    }
+}
+
+vtest::WLClient::WLClient() : m_buffer(nullptr) {
+    if (s_compositor == nullptr) {
+        throw std::runtime_error("Instance is not initialised properly.");
+    }
+
+    m_sharedFile = -468;
+    m_wlSurface  = wl_compositor_create_surface(s_compositor);
+    m_xdgSurface = zxdg_shell_v6_get_xdg_surface(s_xdgShell, m_wlSurface);
+};
+
+vtest::WLClient::~WLClient() {
+    wl_surface_destroy(m_wlSurface);
+
+    if (m_sharedFile >= 0) {
+        close(m_sharedFile);
+    }
+
+    if (m_pool != nullptr) {
+        wl_shm_pool_destroy(m_pool);
+    }
+
+    if (m_buffer != nullptr) {
+        wl_buffer_destroy(m_buffer);
+    }
+}
+
+unsigned char *vtest::WLClient::getBufferData() {
+    int width = 200;
+    int height = 200;
+    int stride = width * 4;
+    int size = stride * height;
+
+    long fd = syscall(SYS_memfd_create, "buffer", 0);
+    m_sharedFile = static_cast<int>(fd) ;
+
+    ftruncate(m_sharedFile, size);
+
+    // turn it into a shared memory pool
+    auto raw_data = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, m_sharedFile, 0);
+
+    m_pool = wl_shm_create_pool(s_sharedMem, m_sharedFile, size);
+    m_buffer = wl_shm_pool_create_buffer(m_pool, 0, width, height, stride, WL_SHM_FORMAT_ARGB8888);
+
+    wl_display_roundtrip(s_display);
+
+    wl_surface_attach(m_wlSurface, m_buffer, 0, 0);
+    wl_surface_commit(m_wlSurface);
+
+    return reinterpret_cast<unsigned char*>(raw_data);
+}
+
+void vtest::WLClient::dispatch() {
+    wl_display_dispatch(s_display);
+}
+
+vtest::XDGSurface *vtest::WLClient::getXDGSurface() {
+    return m_xdgSurface;
+}
+
+vtest::XDGTopLevel* vtest::WLClient::getXDGTopLevel() {
+    return zxdg_surface_v6_get_toplevel(m_xdgSurface);
+}
+
+void vtest::WLClient::commitSurface() {
+    wl_surface_commit(m_wlSurface);
+}
