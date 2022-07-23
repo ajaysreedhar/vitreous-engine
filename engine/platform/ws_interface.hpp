74898a23cf848854cc93eef5d9f76c827d7d0d23
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-07-23 11:06:23 +0530

    chore: Move WSIWindowEvent struct here

---
+++ engine/platform/ws_interface.hpp
@@ -0,0 +1,44 @@
+/**
+ * ws_interface.hpp - Windowing System Interface abstraction.
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2022 Ajay Sreedhar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================================================================
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace vtrs {
+
+struct wsi_window_event {
+    enum EventKind: unsigned int {
+        KEY_PRESS = 10,
+        BUTTON_PRESS,
+        CLOSE_BUTTON_PRESS,
+        EMPTY_EVENT
+    };
+
+    unsigned int kind = EMPTY_EVENT;
+
+    uint32_t eventWindow = -468;
+    uint32_t eventDetail = -468;
+};
+
+typedef struct wsi_window_event WSIWindowEvent;
+
+} // namespace vtrs
