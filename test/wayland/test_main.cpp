c9b1754568a51d3a7b2a6be17d2b3810599e98d8
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-07-25 09:38:38 +0530

    add: Include xdg_shell from third party include dir

--- test/wayland/test_main.cpp
+++ test/wayland/test_main.cpp
@@ -1,95 +1,57 @@
-#include <iostream>
 #include <unistd.h>
-
-#include <wayland-client.h>
+#include <thread>
+#include "platform/except.hpp"
 #include "platform/logger.hpp"
-#include "wl_client.hpp"
+#include "platform/wayland_client.hpp"
 
-void xdg_surface_configure_handler
-        (
-                void *data,
-                struct zxdg_surface_v6 *xdg_surface,
-                uint32_t serial
-        ) {
-    vtrs::Logger::info("Ack surface configure");
-    zxdg_surface_v6_ack_configure(xdg_surface, serial);
-}
+void threadRunner(uint32_t* pixels) {
+    uint32_t color = 0x0000FF;
 
-const struct zxdg_surface_v6_listener xdg_surface_listener = {
-        .configure = xdg_surface_configure_handler
-};
-
-//////////////////////////////////////////////////////////
-
-void xdg_toplevel_configure_handler
-        (
-                void *data,
-                struct zxdg_toplevel_v6 *xdg_toplevel,
-                int32_t width,
-                int32_t height,
-                struct wl_array *states
-        ) {
-    vtrs::Logger::info("configure toplevel:", width, height);
-}
-
-void xdg_toplevel_close_handler
-        (
-                void *data,
-                struct zxdg_toplevel_v6 *xdg_toplevel
-        ) {
-    vtrs::Logger::info("Close TopLevel");
-}
-
-const struct zxdg_toplevel_v6_listener xdg_toplevel_listener = {
-        .configure = xdg_toplevel_configure_handler,
-        .close = xdg_toplevel_close_handler
-};
-
-int main() {
-    vtrs::Logger::info("Test: Hello-Wayland");
-
-    vtest::WLClient* client = vtest::WLClient::getInstance();
-    zxdg_surface_v6_add_listener(client->getXDGSurface(), &xdg_surface_listener, nullptr);
-    zxdg_toplevel_v6_add_listener(client->getXDGTopLevel(), &xdg_toplevel_listener, nullptr);
-
-    client->commitSurface();
-
-    int width = 200;
-    int height = 200;
-    int stride = width * 4;
-
-    unsigned char* buffer_data = client->getBufferData();
-
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-
-            auto pixel = reinterpret_cast<vtest::RGBAPixel*>(buffer_data + y * stride + x * 4);
-
-            // draw a cross
-            if ((80 < x && x < 120) || (80 < y && y < 120)) {
-                pixel->alpha = 255;
-                pixel->red = y / height * 255;
-                pixel->green = pixel->red;
-                pixel->blue = 255;
-            } else {
-                // transparent
-                pixel->alpha = 0;
+    while (color < 0xFFFFFF) {
+        for (int y = 0; y < 480; ++y) {
+            for (int x = 0; x < 640; ++x) {
+                if ((x + y / 8 * 8) % 16 < 8)
+                    pixels[y * 480 + x] = color;
+                else
+                    pixels[y * 480 + x] = 0xFF0000;
             }
         }
+
+        color = color + 0x0000FF;
+        sleep(1);
+        vtrs::Logger::info("Color", color);
+    }
+}
+
+int main() {
+    vtrs::Logger::info("Test: Wayland Client");
+    vtrs::WaylandClient* client;
+
+    try {
+        client = vtrs::WaylandClient::factory();
+        client->createSurface("Wayland Test");
+        vtrs::WaylandClient::displayDispatch();
+
+    } catch (vtrs::PlatformError& error) {
+        vtrs::Logger::error(error.getKind(), error.getKind(), error.getCode());
     }
 
-    client->commitSurface();
+    auto pixels = reinterpret_cast<uint32_t*>(client->getRawPixels());
 
-    vtrs::Logger::info("Preparing to dispatch");
+    if (pixels == nullptr){
+        vtrs::Logger::error("No pixel data!");
+        return EXIT_FAILURE;
+    }
 
-    for(int loop = 0; loop < 2; loop++) {
-        vtrs::Logger::info("Ping", loop);
-        vtest::WLClient::dispatch();
-        sleep(1);
+    std::thread t(threadRunner, pixels);
+
+    while (vtrs::WaylandClient::displayDispatch()) {
+        vtrs::Logger::debug("Dispatch");
+        client->render();
     }
 
     delete client;
 
-    vtest::WLClient::destroy();
+    vtrs::WaylandClient::shutdown();
     return 0;
 }
