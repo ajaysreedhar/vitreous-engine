f83ae31333dce167e409c1ac21c55c3b68802af4
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-08-12 17:15:20 +0530

    add: Print glslc compile flags

---
+++ test/vulkan_apps/shaders/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 'glslc test/vulkan_apps/shaders/triangle.vert -o dist/test/shaders/triangle-vert.spv'
+echo 'glslc test/vulkan_apps/shaders/triangle.frag -o dist/test/shaders/triangle-frag.spv'
