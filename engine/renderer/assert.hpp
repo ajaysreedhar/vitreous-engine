de53c615efb276ff6fef11f170fe0a479bc9f1cd
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-08-28 14:49:13 +0530

    change: Runtime error to RendererError in assertions

--- engine/renderer/assert.hpp
+++ engine/renderer/assert.hpp
@@ -26,5 +26,5 @@
 
 #define VTRS_ASSERT_VK_RESULT(result, message) \
 if (result != VK_SUCCESS) { \
-    throw vtrs::RuntimeError(message, vtrs::RuntimeError::E_TYPE_VK_RESULT, result); \
+    throw vtrs::RendererError(message, vtrs::RuntimeError::E_TYPE_VK_RESULT, result); \
 }
